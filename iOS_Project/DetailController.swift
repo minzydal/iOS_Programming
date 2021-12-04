b0d5fad8c3992ce30f07e86e8e9c2fca2b7d8169
Minzy <minzydal@gmail.com>
2021-12-04 18:42:41 +0900

    Change ViewController, Navigation Bar

---
+++ iOS_Project/DetailController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailController.swift
+//  iOS_Project
+//
+//  Created by MINJI KIM on 2021/12/04.
+//
+
+import UIKit
+
+class DetailController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- iOS_Project/ViewController.swift
+++ iOS_Project/ViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func Click_moveBtn(_ sender: Any) {
+        //storyboard find controller : DetailController
+        
+        //옵셔널 바인딩
+        if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController"){
+        
+        //push controller = navi
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    // button event
-    @IBAction func Move_eventBtn(_ sender: Any) {
-        print("Hello Move Button?")
     }
 }
 
