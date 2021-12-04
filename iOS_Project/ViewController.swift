2cd2f24ca61a86f0b70d15dff0af60c9d469996d
Minzy <minzydal@gmail.com>
2021-12-04 17:47:23 +0900

    Initial Commit

---
+++ iOS_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Project
+//
+//  Created by MINJI KIM on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
